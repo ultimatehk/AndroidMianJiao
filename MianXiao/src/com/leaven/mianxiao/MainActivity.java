77f49367e70f77245711997754bf985e3b2def0c
jiazhen <wangjiazhen@qufenqi.com>
2015-06-02 18:18:32 +0800

    delete v7包

--- MianXiao/src/com/leaven/mianxiao/MainActivity.java
+++ MianXiao/src/com/leaven/mianxiao/MainActivity.java
@@ -1,9 +1,9 @@
 package com.leaven.mianxiao;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
