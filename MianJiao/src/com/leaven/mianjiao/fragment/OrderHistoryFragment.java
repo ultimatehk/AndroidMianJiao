5391fc878e37900f4ad2d9333345e5b8940d39a4
jiazhen <wangjiazhen@qufenqi.com>
2015-06-10 17:12:26 +0800

    fix bug

--- MianJiao/src/com/leaven/mianjiao/fragment/OrderHistoryFragment.java
+++ MianJiao/src/com/leaven/mianjiao/fragment/OrderHistoryFragment.java
@@ -11,7 +11,7 @@ import android.view.ViewGroup;
 public class OrderHistoryFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.fragment_user_info, null);
+		View v = inflater.inflate(R.layout.fragment_order_history, null);
 		return v;
 	}
 }
