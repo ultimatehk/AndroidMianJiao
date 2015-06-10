66851340a90043d3faa9179eff31599ac37dfcb0
jiazhen <wangjiazhen@qufenqi.com>
2015-06-10 17:55:39 +0800

    add 关于我们

--- MianJiao/src/com/leaven/mianjiao/fragment/AboutUsFragment.java
+++ MianJiao/src/com/leaven/mianjiao/fragment/AboutUsFragment.java
@@ -1,6 +1,8 @@
 package com.leaven.mianjiao.fragment;
 
 import com.leaven.mianjiao.R;
+import com.leaven.mianjiao.tools.CommonUtils;
+import com.leaven.mianjiao.view.LeftHeadLineView;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,10 +10,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class AboutUsFragment extends Fragment {
+public class AboutUsFragment extends Fragment implements View.OnClickListener {
+	private LeftHeadLineView headLine;
+	private View tvAboutUs;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_about_us, null);
+		headLine = (LeftHeadLineView) v.findViewById(R.id.headLine);
+		tvAboutUs = v.findViewById(R.id.tvAboutUs);
+		headLine.setOnClickListener(this);
 		return v;
+	}
+
+	@Override
+	public void onClick(View v) {
+		CommonUtils.hideSoftInputFromWindow(v);
+		switch (v.getId()) {
+		case R.id.headLine:
+			tvAboutUs.setVisibility(tvAboutUs.isShown() ? View.GONE : View.VISIBLE);
+			break;
+		}
+
 	}
 }
