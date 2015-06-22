e78ab672bf7429cf796e22a260f5da1ed2b7cbf6
GuoGuo <641176774@qq.com>
2015-06-22 22:04:12 +0800

    fix MapActivity格式化

--- MianJiao/src/com/leaven/mianjiao/MapActivity.java
+++ MianJiao/src/com/leaven/mianjiao/MapActivity.java
@@ -2,39 +2,40 @@ package com.leaven.mianjiao;
 
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.MapView;
+import com.leaven.mianjiao.tools.CommonUtils;
 
 import android.os.Bundle;
 
-public class MapActivity extends BaseActivity{
+public class MapActivity extends BaseActivity {
 	MapView mMapView = null;
-	
+
 	@Override
 	protected void onCreate(Bundle arg0) {
-		// TODO Auto-generated method stub
 		super.onCreate(arg0);
-		SDKInitializer.initialize(getApplicationContext()); 
-		setContentView(R.layout.activity_map); 
+		CommonUtils.initBaiduMap(this);
+		setContentView(R.layout.activity_map);
 		mMapView = (MapView) findViewById(R.id.bmapView);
-		
-		
-	}
-	  @Override  
-	    protected void onDestroy() {  
-	        super.onDestroy();  
-	        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理  
-	        mMapView.onDestroy();  
-	    }  
-	    @Override  
-	    protected void onResume() {  
-	        super.onResume();  
-	        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理  
-	        mMapView.onResume();  
-	        }  
-	    @Override  
-	    protected void onPause() {  
-	        super.onPause();  
-	        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理  
-	        mMapView.onPause();  
-	        }  
-	    }
 
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// 在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+		mMapView.onDestroy();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// 在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+		mMapView.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// 在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+		mMapView.onPause();
+	}
+}
